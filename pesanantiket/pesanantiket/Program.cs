f5f7222288db375a84595f89c921a30451278198
HeniSapta <152345387+HeniSapta@users.noreply.github.com>
2024-02-02 11:39:12 +0700

    Add files via upload

---
+++ pesanantiket/pesanantiket/Program.cs
@@ -0,0 +1,46 @@
+﻿using System; 
+
+public class Person
+{
+    public string Nama { get; set; }
+
+    public string NIK { get; set; }
+    public string NoTelp { get; set; }
+    public string NomorKartuVaksin { get; set; }
+    public string Alamat { get; set; }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine("====== Data Pesan Tiket ======");
+        Console.WriteLine("Nama                 : " + Nama);
+        Console.WriteLine("NIK                  : " + NIK);
+        Console.WriteLine("Nomor Telp           : " + NoTelp);
+        Console.WriteLine("Nomor Kartu Vaksin   : " + NomorKartuVaksin);
+        Console.WriteLine("Alamat               : " + Alamat);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Person person = new Person();
+
+        Console.Write("Masukkan Nama: ");
+        person.Nama = Console.ReadLine();
+
+        Console.Write("Masukkan NIK: ");
+        person.NIK = Console.ReadLine();
+
+        Console.Write("Masukkan Nomor Telepon: ");
+        person.NoTelp = Console.ReadLine();
+
+        Console.Write("Masukkan Nomor Kartu Vaksin: ");
+        person.NomorKartuVaksin = Console.ReadLine();
+
+        Console.Write("Masukkan Alamat: ");
+        person.Alamat = Console.ReadLine();
+
+        person.DisplayInfo();
+    }
+}
